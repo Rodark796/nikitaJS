9dee94c109d270ddbbfd59b515d6a2ae338b5522
Rodark796 <nikita.gluzman@gmail.com>
2022-09-19 19:21:08 +0300

    jsNikita

---
+++ script.js
@@ -0,0 +1,40 @@
+function a() {
+    return 1 < 2 
+}
+
+function b() {
+    return 1 > 2 
+}
+
+function c() {
+    return 1 >= 2 
+}
+
+function d() {
+    return 1 <= 2 
+}
+
+function x() {
+    return 1 === 2 
+}
+
+
+console.log (a() , b() , c() , d() , x())
+
+
+function sum(num1 , num2) {
+    return num1 + num2
+}
+
+let result = sum(1, 2)
+
+console.log(result)
+
+function abc() {
+    let abc = 2
+    return abc * 2
+    let result = abc
+}
+
+
+console.log(abc())
